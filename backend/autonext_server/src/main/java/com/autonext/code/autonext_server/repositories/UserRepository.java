a15fbcbe879177ac11810c68a71c900644a693ec
Alejandro Barrionuevo Rosado <barrionuevorosadoalejandro@gmail.com>
2025-03-13 09:38:02 +0100

    New Seeder

--- backend/autonext_server/src/main/java/com/autonext/code/autonext_server/repositories/UserRepository.java
+++ backend/autonext_server/src/main/java/com/autonext/code/autonext_server/repositories/UserRepository.java
@@ -1,7 +1,10 @@
 package com.autonext.code.autonext_server.repositories;
+
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.autonext.code.autonext_server.models.User;
 
+@Repository
 public interface UserRepository extends CrudRepository<User, Integer> {
 }
