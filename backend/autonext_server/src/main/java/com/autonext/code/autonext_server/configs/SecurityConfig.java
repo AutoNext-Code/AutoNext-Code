65822f56b57a8829aec1636a7d814fd8643af056
Rlopber <ralober92@gmail.com>
2025-03-13 08:41:57 +0100

    Resolve problems with dependencies

---
+++ backend/autonext_server/src/main/java/com/autonext/code/autonext_server/configs/SecurityConfig.java
@@ -0,0 +1,38 @@
+package com.autonext.code.autonext_server.configs;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+  @Autowired
+  private AuthenticationConfiguration authenticationConfiguration;
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http
+        .authorizeHttpRequests(auth -> auth
+            .requestMatchers("/api/auth/**").permitAll()
+            .anyRequest().authenticated())
+        .csrf(csrf -> csrf.disable());
+    return http.build();
+  }
+
+  @Bean
+  AuthenticationManager authenticationManager() throws Exception {
+    return authenticationConfiguration.getAuthenticationManager();
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+}
