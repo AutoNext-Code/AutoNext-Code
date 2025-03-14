e7d3d84c4c0106936e1b5f593fe021c708a2e2ec
Rlopber <ralober92@gmail.com>
2025-03-14 12:49:24 +0100

    Add - Custom Button

--- frontend/autonext-client/src/app/auth/pages/login/login.component.ts
+++ frontend/autonext-client/src/app/auth/pages/login/login.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { CustomButtonComponent } from '../../../shared/custom-button/custom-button.component';
+
+
 
 @Component({
   selector: 'auth-login',
-  imports: [],
+  imports: [CustomButtonComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+  login() {
+    console.log('Login');
+  }
 }
