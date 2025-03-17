c3c39ff10d38e019608e13b9da5bbc9e07de496f
Alvalogue72 <alvalogue72@gmail.com>
2025-03-17 08:18:07 +0100

    Routing Home and AdminHome

---
+++ frontend/autonext-client/src/app/admin/pages/home-admin-page/home-admin-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../../../shared/header/header.component';
+
+@Component({
+  imports: [HeaderComponent],
+  templateUrl: './home-admin-page.component.html',
+  styleUrl: './home-admin-page.component.css'
+})
+export class HomeAdminPageComponent {
+
+}
--- frontend/autonext-client/src/app/app.routes.ts
+++ frontend/autonext-client/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'home',
+        loadComponent: () => import('./user/pages/home-user-page/home-user-page.component').then(m => m.HomeUserPageComponent)
+    },
+    {
+        path: 'admin-home',
+        loadComponent: () => import('./admin/pages/home-admin-page/home-admin-page.component').then(m => m.HomeAdminPageComponent)
+    },
+    {
+        path: '**',
+        redirectTo: 'home'
+    }
+];
