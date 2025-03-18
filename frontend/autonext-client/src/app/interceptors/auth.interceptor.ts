42a2c44fc00bd0115f6454219fa9d7e026b5eee8
Rlopber <ralober92@gmail.com>
2025-03-18 12:05:24 +0100

    Change to use token$

--- frontend/autonext-client/src/app/interceptors/auth.interceptor.ts
+++ frontend/autonext-client/src/app/interceptors/auth.interceptor.ts
@@ -1,17 +1,19 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
+import { switchMap } from 'rxjs/operators';
 import { AuthService } from '../auth/services/auth.service';
-
 
 export const AuthInterceptor: HttpInterceptorFn = (req, next) => {
   const authService = inject(AuthService);
-  const token = authService.getToken();
 
-  if (token) {
-    req = req.clone({
-      setHeaders: { Authorization: `Bearer ${token}` }
-    });
-  }
-
-  return next(req);
+  return authService.token$.pipe(
+    switchMap((token) => {
+      if (token) {
+        req = req.clone({
+          setHeaders: { Authorization: `Bearer ${token}` },
+        });
+      }
+      return next(req);
+    })
+  );
 };
