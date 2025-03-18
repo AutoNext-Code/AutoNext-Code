c5415d67eaa3e52a431010b6558feea8bcebee30
Alejandro Barrionuevo Rosado <barrionuevorosadoalejandro@gmail.com>
2025-03-18 10:53:39 +0100

    Logout

--- frontend/autonext-client/src/app/shared/header/header.component.ts
+++ frontend/autonext-client/src/app/shared/header/header.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CustomButtonComponent } from "../custom-button/custom-button.component";
+import { AuthService } from '../../auth/services/auth.service';
 
 @Component({
   selector: 'shared-header',
@@ -10,12 +11,21 @@ import { CustomButtonComponent } from "../custom-button/custom-button.component"
 })
 export class HeaderComponent {
 
+  private authService: AuthService = inject(AuthService)
+
   menuOpen = false;
+
+  // constructor(private authService: AuthService) {}
 
   constructor() {}
 
   toggleMenu() {
     this.menuOpen = !this.menuOpen;
+  }
+
+  public logout(): void {
+    console.log("Logout")
+    this.authService.logout()
   }
 
 }
