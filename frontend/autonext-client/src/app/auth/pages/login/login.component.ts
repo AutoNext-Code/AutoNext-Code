691b638bcb52951b50919e31b08b509541339b78
Alejandro Barrionuevo Rosado <barrionuevorosadoalejandro@gmail.com>
2025-03-18 12:26:11 +0100

    unfinished Login

--- frontend/autonext-client/src/app/auth/pages/login/login.component.ts
+++ frontend/autonext-client/src/app/auth/pages/login/login.component.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CustomButtonComponent } from '../../../shared/custom-button/custom-button.component';
 import { InputComponent } from "../../../shared/components/ui/input/input.component";
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { AuthCardComponent } from "../../components/layouts/auth-card/auth-card.component";
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'auth-login',
@@ -13,7 +16,23 @@ import { AuthCardComponent } from "../../components/layouts/auth-card/auth-card.
 })
 export class LoginComponent {
 
+  private authService: AuthService = inject(AuthService);
+  private router: Router = inject(Router);
+
+  email: string = "";
+  password: string = "";
+
+  loginResponse$: Observable<string | null> | null = null
+
+  constructor( ) {}
+
   login() {
     console.log('Login');
+
+    console.log(this.email, this.password)
+
+    this.loginResponse$ = this.authService.login(this.email, this.password);
+
+
   }
 }
