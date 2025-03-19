19e01ac68a79e9242d9c0cdd97ab030bdc0fedf4
Alejandro Barrionuevo Rosado <barrionuevorosadoalejandro@gmail.com>
2025-03-19 13:32:53 +0100

    Timeout

--- frontend/autonext-client/src/app/auth/pages/email-confirmation/email-confirmation.component.ts
+++ frontend/autonext-client/src/app/auth/pages/email-confirmation/email-confirmation.component.ts
@@ -1,27 +1,37 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { AuthCardComponent } from "../../components/layouts/auth-card/auth-card.component";
 
 @Component({
+  selector: 'app-email-confirmation',
+  standalone: true,
   imports: [AuthCardComponent],
   templateUrl: './email-confirmation.component.html',
-  styleUrl: './email-confirmation.component.css'
+  styleUrls: ['./email-confirmation.component.css']
 })
-export class EmailConfirmationComponent implements OnInit {
+export class EmailConfirmationComponent implements OnInit, OnDestroy {
 
   private tokenEmail: string = "";
-
   public isConfirmation: boolean;
-
-  private route = inject(ActivatedRoute)
+  private route = inject(ActivatedRoute);
+  private closeTimeout: any;
 
   constructor() {
-    this.isConfirmation = true;
+    this.isConfirmation = false;
   }
 
   ngOnInit(): void {
     this.tokenEmail = this.route.snapshot.paramMap.get("token") || "";
-    console.log("Token email: ", this.tokenEmail)
+    console.log("Token email: ", this.tokenEmail);
+
+    this.closeTimeout = setTimeout(() => {
+      window.close();
+    }, 1000);
   }
 
+  ngOnDestroy(): void {
+    if (this.closeTimeout) {
+      clearTimeout(this.closeTimeout);
+    }
+  }
 }
