1a7b9d4864470e45844aa76c24dda02d87b5d175
Alejandro Barrionuevo Rosado <barrionuevorosadoalejandro@gmail.com>
2025-03-20 13:22:13 +0100

    JwtService protected from token changes

--- backend/autonext_server/src/main/java/com/autonext/code/autonext_server/services/JwtService.java
+++ backend/autonext_server/src/main/java/com/autonext/code/autonext_server/services/JwtService.java
@@ -1,6 +1,7 @@
 package com.autonext.code.autonext_server.services;
 
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
@@ -9,6 +10,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
+import com.autonext.code.autonext_server.exceptions.InvalidJwtException;
 import com.autonext.code.autonext_server.models.User;
 
 import java.util.Date;
@@ -29,18 +31,25 @@ public class JwtService {
         .claim("name", user.getName())
         .issuedAt(new Date())
         .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
+        // Firmado de la clave secreta
         .signWith(getSigningKey())
         .compact();
   }
 
   private <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
-    final Claims claims = Jwts.parser()
-        .verifyWith(getSigningKey())
-        .build()
-        .parseSignedClaims(token)
-        .getPayload();
+    try {
+      final Claims claims = Jwts.parser()
+      // Verifica el token con la clave
+      .verifyWith(getSigningKey())
+      .build()
+      .parseSignedClaims(token)
+      .getPayload();
 
-    return claimsResolver.apply(claims);
+  return claimsResolver.apply(claims);
+    } catch (JwtException e) {
+      throw new InvalidJwtException("Token inválido o expirado");
+    }
+
   }
 
   public String extractUsername(String token) {
