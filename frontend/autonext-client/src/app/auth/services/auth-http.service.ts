9bc8fc5544ff43aaf44436e1ede07eaa2d463baa
Alvalogue72 <alvalogue72@gmail.com>
2025-03-20 13:46:52 +0100

    Add request confirmEmail

--- frontend/autonext-client/src/app/auth/services/auth-http.service.ts
+++ frontend/autonext-client/src/app/auth/services/auth-http.service.ts
@@ -22,4 +22,8 @@ export class AuthHttpService {
     return this.http.post<string>(REGISTER_ENDPOINT, body);
   }
 
+  confirmEmail(token: string): Observable<string> {
+    return this.http.put<string>(`${REGISTER_ENDPOINT}/${token}`, {});
+  }
+
 }
