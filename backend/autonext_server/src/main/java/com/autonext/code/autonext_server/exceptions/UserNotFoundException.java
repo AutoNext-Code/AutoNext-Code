ace6c4124ba18519bda0be3e7f6cec6acf1e807d
Rlopber <ralober92@gmail.com>
2025-03-21 10:10:31 +0100

    Added ResponeStatus

--- backend/autonext_server/src/main/java/com/autonext/code/autonext_server/exceptions/UserNotFoundException.java
+++ backend/autonext_server/src/main/java/com/autonext/code/autonext_server/exceptions/UserNotFoundException.java
@@ -4,8 +4,8 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.http.HttpStatus;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
-public class InvalidJwtException extends RuntimeException {
-    public InvalidJwtException(String message) {
-        super(message);
-    }
+public class UserNotFoundException extends RuntimeException {
+  public UserNotFoundException(String message) {
+    super(message);
+  }
 }
