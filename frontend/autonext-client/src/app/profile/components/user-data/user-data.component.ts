b4618534d51e09afe6860c29868e9e9be5e13309
Alejandro Barrionuevo Rosado <barrionuevorosadoalejandro@gmail.com>
2025-03-27 16:39:37 +0100

    User data unfinished

---
+++ frontend/autonext-client/src/app/profile/components/user-data/user-data.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'profile-user-data',
+  imports: [],
+  templateUrl: './user-data.component.html',
+  styleUrl: './user-data.component.css'
+})
+export class UserDataComponent {
+
+  user = {
+    name: "User user",
+    email: "user@example.com",
+    jobPosition: "developer",
+    workCenterName: "Málaga",
+    strikes: 1,
+    role: "user"
+  }
+
+
+}
