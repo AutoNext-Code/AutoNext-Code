beab22b1ea69a10f7fafb6f0c499535122152da6
Alejandro Barrionuevo Rosado <barrionuevorosadoalejandro@gmail.com>
2025-03-28 13:18:18 +0100

    profile without logic

--- frontend/autonext-client/src/app/profile/components/user-data/user-data.component.ts
+++ frontend/autonext-client/src/app/profile/components/user-data/user-data.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject, NgZone, AfterViewInit, OnDestroy } from '@angular/core';
+import { CustomButtonComponent } from "../../../shared/components/ui/custom-button/custom-button.component";
+import { Router } from '@angular/router';
+import { AppComponent } from '../../../app.component';
 
 @Component({
   selector: 'profile-user-data',
   imports: [],
   templateUrl: './user-data.component.html',
-  styleUrl: './user-data.component.css'
+  styleUrls: ['./user-data.component.css']
 })
-export class UserDataComponent {
+export class UserDataComponent implements AfterViewInit, OnDestroy {
+
+  private router: Router = inject(Router);
+  private ngZone: NgZone = inject(NgZone);
+    private appComponent: AppComponent = inject(AppComponent);
 
   user = {
     name: "User user",
@@ -16,5 +23,29 @@ export class UserDataComponent {
     strikes: 1,
   }
 
+  isMobile = false;
 
+  public navigate(path: string): void {
+    console.log(path);
+    this.router.navigate([path]);
+  }
+
+  public warning(): void {
+    this.appComponent.showToast('warn', 'No implementado', "", 3000, 80);
+  }
+
+  ngAfterViewInit() {
+    this.checkScreenSize();
+    window.addEventListener('resize', this.checkScreenSize.bind(this));
+  }
+
+  checkScreenSize = () => {
+    this.ngZone.run(() => {
+      this.isMobile = window.innerWidth < 768;
+    });
+  };
+
+  ngOnDestroy() {
+    window.removeEventListener('resize', this.checkScreenSize);
+  }
 }
