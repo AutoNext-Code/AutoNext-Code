ba62d5cff8f264927e5647812ced920e80becdde
Rlopber <ralober92@gmail.com>
2025-03-28 13:33:34 +0100

    Booking component

--- frontend/autonext-client/src/app/booking/pages/booking-page/booking-page.component.ts
+++ frontend/autonext-client/src/app/booking/pages/booking-page/booking-page.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '@shared/header/header.component';
 import { BookingHistoryComponent } from '../../components/booking-history/booking-history.component';
+import { SelectDepComponent } from "../../../shared/components/ui/select-dep/select-dep.component";
 
 @Component({
   selector: 'app-booking-page',
