dd60efd945137402462d95384452c8a5b022e7de
Rlopber <ralober92@gmail.com>
2025-03-31 11:00:15 +0200

    Added bean

--- backend/autonext_server/src/main/java/com/autonext/code/autonext_server/configs/WebConfig.java
+++ backend/autonext_server/src/main/java/com/autonext/code/autonext_server/configs/WebConfig.java
@@ -1,13 +1,21 @@
 package com.autonext.code.autonext_server.configs;
 
+import java.io.File;
+
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
   @Override
   public void addResourceHandlers(ResourceHandlerRegistry registry) {
-    registry.addResourceHandler("/*images/**")
-        .addResourceLocations("file:uploads/")
-        .setCachePeriod(3600); // Cache for 1 hour
+    // Imprimir ruta absoluta de la carpeta 'uploads'
+    String absolutePath = new File("uploads/").getAbsolutePath();
+    System.out.println("🔧 Sirviendo imágenes desde: " + absolutePath);
+
+    registry.addResourceHandler("/images/**")
+            .addResourceLocations("file:uploads/")
+            .setCachePeriod(3600);
   }
 }
