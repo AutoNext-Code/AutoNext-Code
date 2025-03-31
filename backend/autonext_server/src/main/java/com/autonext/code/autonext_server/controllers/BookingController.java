e25b1f0a444a8dbef45a1ab9eeb95ec4789c7bd9
Alejandro Barrionuevo Rosado <barrionuevorosadoalejandro@gmail.com>
2025-03-31 15:52:02 +0200

    Fix Error 2

--- backend/autonext_server/src/main/java/com/autonext/code/autonext_server/controllers/BookingController.java
+++ backend/autonext_server/src/main/java/com/autonext/code/autonext_server/controllers/BookingController.java
@@ -2,7 +2,7 @@ package com.autonext.code.autonext_server.controllers;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.PageReques;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
