1828da0d61158b0419387b4a6380723a92e6a911
Alejandro Barrionuevo Rosado <barrionuevorosadoalejandro@gmail.com>
2025-04-01 09:05:17 +0200

    Select plug type funtional

--- frontend/autonext-client/src/app/maps/components/select-plug-type/select-plug-type.component.ts
+++ frontend/autonext-client/src/app/maps/components/select-plug-type/select-plug-type.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { PlugType } from '@maps/enums/plugType.enum';
 
@@ -9,9 +9,12 @@ import { PlugType } from '@maps/enums/plugType.enum';
   templateUrl: './select-plug-type.component.html',
   styleUrl: './select-plug-type.component.css'
 })
-export class SelectPlugTypeComponent {
+export class SelectPlugTypeComponent implements OnInit {
   public plugTypes: (string | PlugType)[];
   public selectedPlugType: string;
+
+  public selectedPlugTypeValue: number | null = null;
+
 
   constructor() {
     this.plugTypes = Object.values(PlugType)
@@ -19,4 +22,42 @@ export class SelectPlugTypeComponent {
 
     this.selectedPlugType = "Undefined";
   }
+
+  ngOnInit(): void {
+      this.getSelectedPlugTypeValue();
+  }
+
+  get displaySelectedPlugType(): string {
+    return this.selectedPlugType === "Undefined" ? "Todos los cargadores" : this.selectedPlugType;
+  }
+
+  getSelectedPlugTypeValue(): void {
+    switch (this.selectedPlugType) {
+      case "Schuko":
+        this.selectedPlugTypeValue = PlugType.Schuko;
+        break;
+      case "Type1":
+        this.selectedPlugTypeValue = PlugType.Type1;
+        break;
+      case "Type2":
+        this.selectedPlugTypeValue = PlugType.Type2;
+        break;
+      case "CCS":
+        this.selectedPlugTypeValue = PlugType.CCS;
+        break;
+      case "CHAdeMO":
+        this.selectedPlugTypeValue = PlugType.CHAdeMO;
+        break;
+      case "Undefined":
+      default:
+        this.selectedPlugTypeValue = PlugType.Undefined;
+        break;
+    }
+
+    console.log("Selected PlugType:", this.selectedPlugType);
+    console.log("Numeric Value:", this.selectedPlugTypeValue);
+  }
+
+
+
 }
