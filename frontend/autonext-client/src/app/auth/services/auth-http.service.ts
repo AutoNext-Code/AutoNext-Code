e3256072bfb57de3bdcaa3a6e00efcee2dcd4248
Rlopber <ralober92@gmail.com>
2025-04-11 10:49:53 +0200

    feat(auth): add forgot-password and reset-password endpoints to AuthHttpService

--- frontend/autonext-client/src/app/auth/services/auth-http.service.ts
+++ frontend/autonext-client/src/app/auth/services/auth-http.service.ts
@@ -3,7 +3,13 @@ import { inject, Injectable } from '@angular/core';
 
 import { Observable } from 'rxjs';
 
-import { CONFIRM_EMAIL_ENDPOINT, LOGIN_ENDPOINT, REGISTER_ENDPOINT } from '../../config';
+import {
+  CONFIRM_EMAIL_ENDPOINT,
+  FORGET_PASSWORD,
+  LOGIN_ENDPOINT,
+  REGISTER_ENDPOINT,
+  RESET_PASSWORD,
+} from '../../config';
 
 @Injectable({
   providedIn: 'root',
@@ -18,13 +24,28 @@ export class AuthHttpService {
     return this.http.post(LOGIN_ENDPOINT, body, { responseType: 'text' });
   }
 
-  register(email: string, name:string, surname:string, password:string, carPlate: string, ): Observable<string> {
-    const body = { email ,name, surname, password, carPlate };
+  register(
+    email: string,
+    name: string,
+    surname: string,
+    password: string,
+    carPlate: string
+  ): Observable<string> {
+    const body = { email, name, surname, password, carPlate };
     return this.http.post(REGISTER_ENDPOINT, body, { responseType: 'text' });
   }
 
   confirmEmail(token: string): Observable<string> {
-    return this.http.put(CONFIRM_EMAIL_ENDPOINT, token, { responseType: 'text'});
+    return this.http.put(CONFIRM_EMAIL_ENDPOINT, token, {
+      responseType: 'text',
+    });
   }
 
+  requestPasswordReset(email: string): Observable<string> {
+    return this.http.post(FORGET_PASSWORD, { email }, { responseType: 'text' });
+  }
+
+  requestNewPassword(password: string): Observable<string> {
+    return this.http.post(RESET_PASSWORD, { password }, { responseType: 'text' });
+  }
 }
