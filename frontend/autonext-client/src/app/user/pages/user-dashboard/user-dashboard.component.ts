7dae18715d7a68f89de5b1254fb9da010d631743
Manuel Alarcón Fortes <alarmanuel16@gmail.com>
2025-04-22 09:14:28 +0200

    Dashboard Layout Started with Month Select

--- frontend/autonext-client/src/app/user/pages/user-dashboard/user-dashboard.component.ts
+++ frontend/autonext-client/src/app/user/pages/user-dashboard/user-dashboard.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '@shared/header/header.component';
+import { DashboardLayoutComponent } from "../../components/dashboard-layout/dashboard-layout.component";
 
 @Component({
   selector: 'app-user-dashboard',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, DashboardLayoutComponent],
   templateUrl: './user-dashboard.component.html',
   styleUrl: './user-dashboard.component.css'
 })
