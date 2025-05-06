5a2eabac0f8e9f4be51067212da6bd683848e81e
Alvalogue72 <alvalogue72@gmail.com>
2025-05-06 13:32:36 +0200

    User Management View Complete

--- frontend/autonext-client/src/app/admin/components/users-data/users-data.component.ts
+++ frontend/autonext-client/src/app/admin/components/users-data/users-data.component.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { Component, effect, inject, Input, Signal } from '@angular/core';
 import { AppComponent } from '../../../app.component';
 import { HttpErrorResponse } from '@angular/common/http';
+import { JobPosition } from '@admin/enums/jobPosition.enum';
 
 @Component({
   selector: 'admin-users-data',
@@ -19,12 +20,19 @@ export class UsersDataComponent {
   @Input() emailSignal!: Signal<string>;
 
   users: UserForAdmin[] = [];
+  jobPositions = Object.entries(JobPosition)
+  .filter(([key, value]) => isNaN(Number(key)))
+  .map(([key, value]) => ({
+    id: value as number,
+    name: key === 'Undefined' ? 'Sin asignación' : key.replace(/_/g, ' ')
+  }));
 
   constructor() {
     effect(() => {
       const email = this.emailSignal();
       this.loadUsers(email);
     });
+    console.log(this.jobPositions)
   }
 
   getWorkCenterValue(workCenter: string | null): number {
@@ -39,16 +47,44 @@ export class UsersDataComponent {
     }
   }
 
+  getJobPositionValue(jobPosition: string | null): number {
+    switch (jobPosition) {
+      case 'Intern':
+        return 1;
+      case 'Product_Owner':
+        return 2;
+      case 'Scrum_Master':
+        return 3;
+      case 'Developer':
+        return 4;
+      case 'PEM':
+        return 5;
+      case 'Branch_Head':
+        return 6;
+      case null:
+      default:
+        return 0;
+    }
+  }
+
+  formatJobPosition(jobPosition: string): string {
+    return jobPosition.replace(/_/g, ' ');
+  }
+
   loadUsers(email?: string): void {
     this.adminService.getUsers(email).subscribe({
       next: (users) => {
-        this.users = users;
+        this.users = users.map(user => ({
+          ...user,
+          selectedJobPosition: this.getJobPositionValue(user.jobPosition)
+        }));
       },
       error: (error: HttpErrorResponse) => {
         console.error('ERROR AL AAAA al cargar usuarios:', error);
-        this.appComponent.showToast('error', 'Error al obtener usuarios. ' , error.error, 3000);
+        this.appComponent.showToast('error', 'Error al obtener usuarios. ', error.error, 3000);
       },
     });
   }
+  
 
 }
